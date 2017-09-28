a549ede6b02b1814baa719dc2d04895f99afc3fb
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-27 22:50:24 -0400

    practice

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// require( './Components/module_one.css' );
+//--------------------------------------------------
+//==================================================
+class APP extends Component {
+
+    constructor( props ) {
+        super( props );
+        this.state = {};
+    };
+
+    render() {
+        return (
+            
+            <div>
+                hello there!
+            </div>
+        );
+    };
+};
+//==================================================
+ReactDOM.render( <APP />, document.querySelector( '#main' ) );
